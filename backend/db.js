4b79b01561a5ff0e0ea2f5f167e4a900424f38e1
amrin4623 <z.amrin4623@gmail.com>
2026-07-16 18:57:00 +0530

    updated db.js

--- backend/db.js
+++ backend/db.js
@@ -1,7 +1,7 @@
 const mysql = require("mysql2");
 
 const connection = mysql.createConnection({
-    host: "172.31.42.118",
+    host: "172.31.38.219",
     user: "appuser",
     password: "P@ssword",
     database: "appdb"
